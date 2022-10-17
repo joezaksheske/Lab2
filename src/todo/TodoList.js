792af3b47d862bc15bc83e2f1d522f2604daf095
joezaksheske <joseph.zaksheske@gmail.com>
2022-10-17 10:26:21 -0500

    added logic to show message that there were no todos when the todo's array was empty

--- src/todo/TodoList.js
+++ src/todo/TodoList.js
@@ -1,12 +1,15 @@
 import Todo from "./Todo";
 
 export default function TodoList({ todos = [], dispatch }) {
-  return (
-    <div>
-      {console.log(todos)}
-      {todos.map((todo) => (
-        <Todo todo={todo} key={todo.todoID} dispatch={dispatch} />
-      ))}
-    </div>
-  );
+  if (todos.length === 0)
+    {return (<div>No Todos here</div>)}
+  else
+    {return (
+      <div>
+        {console.log(todos)}
+        {todos.map((todo) => (
+          <Todo todo={todo} key={todo.todoID} dispatch={dispatch} />
+        ))}
+      </div>
+  );}
 }
